87521e38c96756b3b6f635da36b9d882a7b3060b
Mariam2Waleed <Mariam2waleed@gmail.com>
2024-07-25 19:16:13 +0300

    All Good

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,14 @@ void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
-  // FlutterNativeSplash.remove();
   initialization();
 }
 
-// Trying to do it
 void initialization() async {
   // This is where you can initialize the resources needed by your app while
   // the splash screen is displayed.  Remove the following example because
   // delaying the user experience is a bad design practice!
   // ignore_for_file: avoid_print
-  // print('ready in 3...');
-  // await Future.delayed(const Duration(seconds: 1));
-  // print('ready in 2...');
-  // await Future.delayed(const Duration(seconds: 1));
   print('ready in 1...');
   await Future.delayed(const Duration(seconds: 1));
   print('go!');
